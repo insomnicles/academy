1f840a8081564f628f1d355cfb45fdb139ea2118
Geo <13597665+github-34@users.noreply.github.com>
2022-12-08 12:33:50 -0500

    Added: cli using click and logging support

--- html2prettyhtml.py
+++ html2prettyhtml.py
@@ -1,20 +1,72 @@
 from prettifier import EasyPrettifier
 from extractors import GutenbergExtractor
 
-from urllib.error import URLError
-from urllib.error import HTTPError
 from urllib.request import urlopen
 from urllib.parse import urlparse
 import time
 import os
-class Html2PrettyHtml:
-    DEBUG = False
+import click
+import logging
 
+@click.command()
+@click.argument('id', default="1643")
+@click.argument('outputdir', default="output/test")
+@click.option('--theme', default="easy", help="Theme. Default is the easy theme.")
+@click.option('--fromlocaldir', help='Process book with **id** from a local directory instead of retrieving from url.')
+@click.option('--all', is_flag=True, help='Process all files in a local directory. Id argument will be ignored.')
+@click.option('--verbose', is_flag=True, help='Get extra information about what\'s happening behind the scenes.')
+@click.option('--debug', is_flag=True, help='Turn on debugging messages.')
+
+def html2prettyhtml(id, outputdir, theme, fromlocaldir, all, verbose, debug):
+    """ html2prettyhtml converts a Project Gutenberg book in html into pretty html reader.
+    The reader is stylized by theme, the default being the "easy" theme.
+    The books are retrieved from Project Guternberg directly or from a local source directory.
+    """
+
+    if verbose:
+        logging.basicConfig(filename='html2prettyhtml.log', filemode="w", level=logging.INFO)
+
+    if debug:
+        logging.basicConfig(filename='html2prettyhtml.log', filemode="w", level=logging.DEBUG)
+
+    themes = [ "easy" ]
+    if theme not in themes:
+        print("Error: theme not recognized. Valid themes: " + ", ".join(themes))
+        return
+
+    if all and not fromlocaldir:
+        print("Error: --fromlocaldir option must be set to use --all\n E.g. python html2prettyhtml.py --fromlocaldir=/src/gutenberg --all")
+        return
+
+    bc = Html2PrettyHtml()
+
+    if all and fromlocaldir:
+        local_dir = fromlocaldir if fromlocaldir[-1] == "/" else fromlocaldir + "/"
+        for path, subdirs, files in os.walk(local_dir):
+            for name in files:
+                if name.endswith(".html"):
+                    bc.convert("gutenberg", os.path.join(path, name), outputdir, theme)
+                    time.sleep(0.25)
+            return
+
+    if fromlocaldir:
+        local_dir = fromlocaldir if fromlocaldir[-1] == "/" else fromlocaldir + "/"
+        file_source = local_dir + "pg" + str(id) + "-images.html"
+        bc.convert("gutenberg", file_source, outputdir, theme)
+        return
+
+    url_source = "https://www.gutenberg.org/cache/epub/" + str(id) + "/" + "pg" + str(id) + "-images.html"
+    bc.convert("gutenberg", url_source, outputdir, theme)
+
+
+class Html2PrettyHtml:
     source, source_work, theme, output_dir = "", "", "", ""
     structured_doc = None
 
-    def convert(self, source, source_work, theme, output_dir):
-        print("\n------------------\nProcessing " + source_work)
+    def convert(self, source, source_work, output_dir, theme):
+        print('Started conversion from file %s.' % source_work)
+        logging.info('Started converting %s.' % source_work)
+
         self.source = source
         self.source_work = source_work
         self.theme = theme
@@ -27,101 +79,30 @@ class Html2PrettyHtml:
                 raise Exception("Could not find a suitable extractor")
             self.structured_doc = ext.extract()
             del ext
-            print("Extracted File Successfully\n") if self.DEBUG else True
-        except HTTPError as http_error:
-            print("Error: could not convert url: " + source_work + " HTTPError")
-            self.reset()
-            return
-        except URLError as url_error:
-            print("Error: could not convert url:" + source_work + " URL Error: Server Not Found")
-            self.reset()
-            return
-        except OSError as e:
-            print("Error: could not convert file/url: " + source_work + ": file/url not found or not readable\n")
-            self.reset()
-            return
-        except Exception as e:
-            print("Error: could not extract data from source file/url\n")
-            print(e)
-            self.reset()
-            return
 
-        try:
             if (theme == "easy"):
                 crt = EasyPrettifier()
             else:
                 raise Exception("Theme " + theme + " was not recognized.")
 
             crt.create(self.structured_doc)
-            filename = self.structured_doc['metadata']['dc.title'] + ".html" if self.structured_doc['metadata']['dc.title'] != "" else self.structured_doc['src']['filename']
+            #filename = self.structured_doc['metadata']['dc.title'] + ".html" if self.structured_doc['metadata']['dc.title'] != "" else self.structured_doc['src']['filename']
+            filename = self.structured_doc['src']['filename']
             crt.save(output_dir, filename)
+
             del crt
-            print("Converted File Successfully: " + filename)
+            print("Converted File Successfully to " + self.output_dir + filename)
+            logging.info('Converted File Successfully to %s.' % self.output_dir + filename)
+            self.reset()
         except Exception as e:
-            print("Error: could not beautify document data")
             print(e)
             self.reset()
             return
 
-        self.reset()
 
     def reset(self):
         self.source, self.source_work, self.theme, self.output_dir = "", "", "", ""
         self.structured_doc = None
 
-plato_urls = {
-    #"alcibiadesI":  "https://www.gutenberg.org/cache/epub/1676/pg1676-images.html",     # ERROR: original missing toc markup
-    "alcibiadesI":  "https://www.gutenberg.org/files/1676/1676-h/1676-h.htm",
-    "alcibiadesII": "https://www.gutenberg.org/cache/epub/1677/pg1677-images.html",     # MINOR: no marcel.tr tag
-    "apology":      "https://www.gutenberg.org/cache/epub/1656/pg1656-images.html",
-    "charmides":    "https://www.gutenberg.org/cache/epub/1580/pg1580-images.html",
-    "cratylus":     "https://www.gutenberg.org/cache/epub/1616/pg1616-images.html",     # Minor: translator in par; descriptions not parsed; add footnotes
-    #"crito":        "https://www.gutenberg.org/cache/epub/1657/pg1657-images.html",     # ERROR: original missing toc markup
-    "critias":      "https://www.gutenberg.org/cache/epub/1571/pg1571-images.html",
-    "euthydemus":   "https://www.gutenberg.org/cache/epub/1598/pg1598-images.html",
-    "euthyphro":    "https://www.gutenberg.org/cache/epub/1642/pg1642-images.html",
-    "eryxias":      "https://www.gutenberg.org/cache/epub/1681/pg1681-images.html",
-    "gorgias":      "https://www.gutenberg.org/cache/epub/1672/pg1672-images.html",
-    "ion":          "https://www.gutenberg.org/cache/epub/1635/pg1635-images.html",
-    "laches":       "https://www.gutenberg.org/cache/epub/1584/pg1584-images.html",     # Minor: PERSONS in TOC
-    "laws":         "https://www.gutenberg.org/cache/epub/1750/pg1750-images.html",
-    "lesser-hippias": "https://www.gutenberg.org/cache/epub/1673/pg1673-images.html",   # ERROR: original missing toc markup
-    "lysis":        "https://www.gutenberg.org/cache/epub/1579/pg1579-images.html",     # Minor: PERSONS toc entry
-    "menexenus":    "https://www.gutenberg.org/cache/epub/1682/pg1682-images.html",     # Minor: PERSONS toc entry
-    "meno":         "https://www.gutenberg.org/cache/epub/1643/pg1643-images.html",
-    "parmenides":   "https://www.gutenberg.org/cache/epub/1687/pg1687-images.html",
-    "phaedo":       "https://www.gutenberg.org/cache/epub/1658/pg1658-images.html",     # Minor: PERSONS OF THE DIALOGUE in dialogue para
-    "phaedrus":     "https://www.gutenberg.org/cache/epub/1636/pg1636-images.html",
-    "philebus":     "https://www.gutenberg.org/cache/epub/1744/pg1744-images.html",
-    "protagoras":   "https://www.gutenberg.org/cache/epub/1591/pg1591-images.html",
-    # "republic2":     "https://www.gutenberg.org/cache/epub/55201/pg55201-images.html",
-    "republic":     "https://www.gutenberg.org/cache/epub/1497/pg1497-images.html",
-    "sophist":      "https://www.gutenberg.org/cache/epub/1735/pg1735-images.html",
-    "statesman":    "https://www.gutenberg.org/cache/epub/1738/pg1738-images.html",
-    "symposium":    "https://www.gutenberg.org/cache/epub/1600/pg1600-images.html",
-    "timaeus":      "https://www.gutenberg.org/cache/epub/1572/pg1572-images.html",
-    "theaetetus":   "https://www.gutenberg.org/cache/epub/1726/pg1726-images.html",
-    "notplato":     "https://www.asdflasdfasdf.com/1.html",
-}
-
-
-bc = Html2PrettyHtml()
-
-# URLs
-for url in plato_urls:
-    bc.convert("gutenberg", plato_urls[url], "easy", "output/gutenberg/urls")
-
-#FILE
-#bc.convert("gutenberg", "src/books/gutenberg/plato/plato-republic-2-tr-jowett-guttenberg.html", "easy", "output/gutenberg/plato")
-
-# FILES
-#src_directory = "src/books/gutenberg/"
-src_directory = "src/books/gutenberg/plato/"
-# src_directory = "src/books/gutenberg/aristotle/"
-# src_directory = "src/books/gutenberg/sophocles"
-
-# for path, subdirs, files in os.walk(src_directory):
-#     for name in files:
-#         if name.endswith(".html"):
-#             bc.convert("gutenberg", os.path.join(path, name), "easy", "output/gutenberg/plato")
-#             time.sleep(1)
+if __name__ == '__main__':
+    html2prettyhtml()
